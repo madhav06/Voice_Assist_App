4fccd5185ca094530cbac57310ca12e7ed8700a5
madhav06 <mnandan06@gmail.com>
2021-03-02 20:06:34 +0530

    +adding Hooks and Styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
 import logo from './logo.svg';
 import './App.css';
 
 const alanKey = '4c3bf7bc85122c276b2a2fa0bfdca40a2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () =>  {
+  const [newsArticles, setNewsArticles] = useState([]);
 
   useEffect(() => {
     alanBtn({
       key: alanKey,
       onCommand: ({command, articles}) => {
         if(command === 'newHeadlines'){
-          console.log(articles);
+          setNewsArticles(articles);
         }
       }
     })
@@ -20,6 +22,7 @@ const App = () =>  {
   return (
     <div className="App">
       <h1>Sophie-AI News Broadcaster</h1>
+      <NewsCards articles={newsArticles}/>
     </div>
   );
 }
