2dbc3c5782accb81312c51632924cd39d78e374d
madhav06 <mnandan06@gmail.com>
2021-03-03 08:08:34 +0530

    +News by Sources, Terms, Categories

--- src/App.js
+++ src/App.js
@@ -1,30 +1,72 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
 import alanBtn from '@alan-ai/alan-sdk-web';
-import NewsCards from './components/NewsCards/NewsCards';
-import logo from './logo.svg';
-import './App.css';
 
-const alanKey = '4c3bf7bc85122c276b2a2fa0bfdca40a2e956eca572e1d8b807a3e2338fdd0dc/stage';
+import logo from './images/logo.png';
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
 
-const App = () =>  {
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
   const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
 
   useEffect(() => {
     alanBtn({
-      key: alanKey,
-      onCommand: ({command, articles}) => {
-        if(command === 'newHeadlines'){
+      key: 'alanKey',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
           setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
         }
-      }
-    })
-  }, [])
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Sophie-AI News Broadcaster</h1>
-      <NewsCards articles={newsArticles}/>
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img src="https://github.com/madhav06/projectImages/blob/master/alan.jpg" className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a className={classes.link} href="https://www.linkedin.com/in/pandit-madhv-a7809a8b/"> Madhav Pandit</a> -
+            <a className={classes.link} href="https://github.com/madhav06/voice_assist_app"> MIT License</a>
+          </Typography>
+          <img className={classes.image} src={logo} height="50px" alt="MIT logo" />
+        </div>
+      ) : null}
     </div>
   );
-}
+};
 
 export default App;
